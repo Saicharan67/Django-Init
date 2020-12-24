8004e0a5a5e8263a9c8635aa7dde0879d9f7e0ef
Saicharan67 <mahankalisaicharan@gmail.com>
2020-12-24 19:02:47 +0530

    added static files

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Destination:
+    id: int
+    percent: int
+    name: str
--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    dest1 = Destination()
+    dest1.name = 'Django'
+    dest1.percent = 40
+    return render(request,'index.html',{'dest1': dest1})
